d443bc18ab9774c85e3129e103e19440a72aae35
RuddyMo <ruddymorel10@gmail.com>
2024-11-29 15:36:58 +0100

    feat(scroll): first commit and add scroll

---
+++ layouts/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-between px-2 py-1 text-white bg-black">
+    <h1>Dylan.</h1>
+    <nav class="flex gap-3">
+      <a>
+        <nuxt-link to="/">Index</nuxt-link>
+      </a>
+      <a>
+        <nuxt-link to="/about">About</nuxt-link>
+      </a>
+      <a>
+        <nuxt-link to="/contact">Contact</nuxt-link>
+      </a>
+    </nav>
+  </div>
+  <slot />
+</template>
---
+++ pages/main.vue
@@ -0,0 +1,8 @@
+<template>
+  <NuxtLayout name="navbar">
+    <imageComponents />
+  </NuxtLayout>
+</template>
+<script setup lang="ts">
+import ImageComponents from '~/components/imageComponents.vue';
+</script>

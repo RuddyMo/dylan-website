74e198f540b22a35f20084bc6c980a827575eabe
RuddyMo <ruddymorel10@gmail.com>
2025-12-18 23:18:33 +0100

    fix error

--- app/layouts/navbar.vue
+++ app/layouts/navbar.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="flex justify-between px-4 text-black bg-white" :class="route.path === '/galerie' ? 'pt-2' : 'py-2'">
       <nuxt-link
-        to="/public"
+        to="/"
         class="text-sm relative px-1 before:inline-block before:content-['.'] before:absolute before:left-0 before:opacity-0 before:translate-x-2 before:transition-all before:duration-300 hover:before:opacity-100 hover:before:translate-x-0"
       >
         <span class="font-semibold">Dylan Morel</span> Photographie.

2b4eed1ab9db89322919036201d7a99642dd1cb0
RuddyMo <ruddymorel10@gmail.com>
2025-12-26 19:02:45 +0100

    delete shadow

--- app/layouts/navbar.vue
+++ app/layouts/navbar.vue
@@ -28,7 +28,7 @@
           contact
         </nuxt-link>
         <nuxt-link v-if="isAuthenticated" to="/dashboard" title="Dashboard"
-          ><Button size="icon" class="h-3 w-3"><ShieldUser :size="17" /></Button
+          ><Button size="icon" class="h-[20px] w-[20px] shadow-none"><ShieldUser :size="20" /></Button
         ></nuxt-link>
       </nav>
     </div>
